fbbc8ce585a4257018b47f141ca0f49e007a50c9
ziggyzacks <zigzacks@gmail.com>
2019-02-24 12:04:14 -0500

    updates

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 ## Experiments with a modern stack
 
-1. kops create cluster --zones us-east-1a --node-count 1 --node-size t3.medium --master-size c5.large --ssh-public-key ssh/zigdata_rsa.pub  zigdata.org
-2. export NAME=zigdata.org export KOPS_STATE_STORE=s3://zigdata-org-state-store kops update cluster ${NAME} --yes
-3. Set up helm for the cluster 
+1. ./bin/cluster-up.sh
+    - wait for cluster to spin up and become available 
+2. Set up helm for the cluster 
     - kubectl create serviceaccount --namespace kube-system tiller                                                                                           1 ⏎  +21048 1:44 ❰─┘
     - kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
     - helm init --service-account tiller
-4. kubectl create -f https://k8s.io/examples/admin/namespace-dev.json
-5. kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml
-6. helm install --debug --name zigdata --namespace development --values values.yaml --values secrets.yaml .
+3. kubectl create -f https://k8s.io/examples/admin/namespace-dev.json
+4. kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml
+5. helm install --debug --name zigdata --namespace development --values values.yaml --values secrets.yaml .
     - anytime the app is redeployed run `python route53.py` to add the elb's dns to the route53 records for that domain
     - will create {elb,app,lab,dev}.zigdata.org subdomains
+6. Update images in cluster
+    - ./bin/update-cluster-images.sh
---
+++ reddit-headline-extractor.py
@@ -0,0 +1,96 @@
+import os
+import sys
+import pandas as pd
+import praw
+import nltk
+import s3fs
+from datetime import datetime
+from time import sleep
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(funcName)s - %(message)s')
+logger = logging.getLogger("reddit")
+logger.setLevel(logging.INFO)
+
+try:
+    nltk.data.find('vader_lexicon')
+except LookupError:
+    nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
+
+CLIENT_ID = os.environ.get('REDDIT_CLIENT_ID')
+CLIENT_SECRET = os.environ.get('REDDIT_CLIENT_SECRET')
+USER_AGENT = os.environ.get('REDDIT_USER_AGENT')
+NAP_TIME = os.environ.get('REDDIT_EXTRACTOR_NAPTIME', 900)
+
+
+class RedditHeadlineExtractor:
+
+    def __init__(self, client_id, client_secret, user_agent):
+        self.reddit = praw.Reddit(client_id=client_id,
+                                  client_secret=client_secret,
+                                  user_agent=user_agent)
+        self.sia = SIA()
+        self.fs = s3fs.S3FileSystem()
+
+    @classmethod
+    def from_environment(cls):
+        return cls(CLIENT_ID, CLIENT_SECRET, USER_AGENT)
+
+    def fetch_headlines_from_s3(self, pattern='s3://zigdata.org/reddit-headlines/*'):
+        headline_files = self.fs.glob(pattern)
+        if len(headline_files) > 0:
+            dfs = []
+            for f in headline_files:
+                logger.info(f'Loading: {f}')
+                dfs.append(pd.read_parquet(f))
+            headlines = pd.concat(dfs)
+            logger.info(f"Found {len(headlines)} headlines in {len(headline_files)} records")
+        else:
+            logger.info('No headlines files found in S3.')
+            headlines = None
+        return headlines
+
+    def gather_headlines(self, subreddit='politics'):
+        new_headlines = set()
+        records = []
+
+        logger.info(f"Fetching new headlines from {subreddit}")
+        for submission in self.reddit.subreddit(subreddit).new(limit=None):
+            title = submission.title
+            if title not in new_headlines:
+                new_headlines.add(title)
+                record = {
+                    "title": title,
+                    "ups": submission.ups,
+                    "downs": submission.downs,
+                    "subreddit": subreddit,
+                    "created_epoch": submission.created_utc,
+                    "ncomments": submission.num_comments
+                }
+                records.append(record)
+        logger.info(f"Found {len(new_headlines)} new headlines in the {subreddit} subreddit")
+        return records
+
+    def build_df_from_headlines(self, headlines):
+        results = []
+        logger.info('Calculating sentiment scores')
+        for record in headlines:
+            pol_score = self.sia.polarity_scores(record['title'])
+            results.append({**record, **pol_score})
+        df = pd.DataFrame.from_records(results)
+        df['timestamp'] = pd.to_datetime(df.created_epoch, unit='s')
+        return df
+
+
+if __name__ == "__main__":
+    extractor = RedditHeadlineExtractor.from_environment()
+    headlines = extractor.gather_headlines()
+    df = extractor.build_df_from_headlines(headlines)
+    logger.info('Persiting to s3')
+    df.to_parquet(f's3://zigdata.org/reddit-headlines/{datetime.now().strftime("%Y-%m-%d_%H%M%S")}.parquet.gzip',
+                  compression='gzip')
+    logger.info(f'Sleeping for {NAP_TIME // 60} minutes')
+    # 15 minutes
+    sleep(NAP_TIME)
+    sys.exit(0)
