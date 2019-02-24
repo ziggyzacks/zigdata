f3786151f85e55aec97c3458928548ce60a1a04e
ziggyzacks <zigzacks@gmail.com>
2019-02-24 12:54:54 -0500

    aws auth and skip the route53 stuff on update until I get the jq better

--- bin/update-cluster-images.sh
+++ bin/update-cluster-images.sh
@@ -3,15 +3,29 @@
 cd zigdata
 # delete release
 helm del --purge zigdata
-sleep 10
 # install
 helm install --debug --name zigdata --namespace development --values values.yaml --values secrets.yaml .
-echo $(helm list)
-kubectl get pods,svc,ing,deploy  --all-namespaces
-sleep 60
 
-cd ..
-# dns
-python bin/route53.py
+echo $(helm list)
+function check_elb {
+    echo $(kubectl get svc -n development -o json | jq '.items | length')
+}
+
+function log {
+    echo "$(date) - $1"
+}
+
+#elb=$(check_elb)
+#while [ $elb -eq 0 ]
+#do
+#    elb=$(check_elb)
+#    log "waiting for ELB.."
+#    sleep 5
+#done
+#log $elb
+#
+## dns
+#cd ../bin
+#python route53.py
 # logs
 stern --since 5m -n development zigdata
