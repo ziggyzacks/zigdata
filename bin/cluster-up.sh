5d4b8cec40365676f8c355403c0fd2860974af89
ziggyzacks <zigzacks@gmail.com>
2019-02-24 16:20:48 -0500

    only make one subdomain for jupyter + condense setup

--- bin/cluster-up.sh
+++ bin/cluster-up.sh
@@ -8,3 +8,11 @@ kops create cluster \
         --ssh-public-key ssh/zigdata_rsa.pub \
         --name $NAME
 kops update cluster ${NAME} --yes
+# helm setup
+kubectl create serviceaccount --namespace kube-system tiller                                                                                           1 ⏎  +21048 1:44 ❰─┘
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+helm init --service-account tiller
+# make a development namespace
+kubectl create -f https://k8s.io/examples/admin/namespace-dev.json
+# dashboard
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml
