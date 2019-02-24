27ac23088fed90c3cd49282a7a705c2d0e51533c
ziggyzacks <zigzacks@gmail.com>
2019-02-24 16:34:22 -0500

    go back to old way

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## Experiments with a modern stack
 
-1. ./cluster-up.sh
-    - wait for cluster to spin up and become available 
-2. ./bin/deploy.sh
+1. ./bin/cluster-up.sh
+    - wait for cluster to spin up and become available
+2. ./bin/bootstrap-cluster.sh
+3. ./bin/deploy.sh
--- bin/cluster-up.sh
+++ bin/cluster-up.sh
@@ -1,5 +1,6 @@
 export KOPS_STATE_STORE=s3://zigdata-org-state-store
 export NAME=zigdata.org
+
 kops create cluster \
         --zones us-east-1a \
         --node-count 1 \
@@ -7,12 +8,5 @@ kops create cluster \
         --master-size c5.large \
         --ssh-public-key ssh/zigdata_rsa.pub \
         --name $NAME
+
 kops update cluster ${NAME} --yes
-# helm setup
-kubectl create serviceaccount --namespace kube-system tiller                                                                                           1 ⏎  +21048 1:44 ❰─┘
-kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-helm init --service-account tiller
-# make a development namespace
-kubectl create -f https://k8s.io/examples/admin/namespace-dev.json
-# dashboard
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml
