daf699b1fe6f808e8e862dc84e8c8e1d521bd755
ziggyzacks <zigzacks@gmail.com>
2019-02-24 00:54:07 -0500

    reload data every 15 min

--- reddit.py
+++ reddit.py
@@ -1,31 +1,33 @@
 import os
-import pickle
+import sys
 import pandas as pd
 
 import praw
 import nltk
+import datetime
+from time import sleep, time
 
 nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 
-from sklearn.feature_extraction.text import CountVectorizer
+while True:
+    reddit = praw.Reddit(client_id=os.environ.get('REDDIT_CLIENT_ID'),
+                         client_secret=os.environ.get('REDDIT_CLIENT_SECRET'),
+                         user_agent=os.environ.get('REDDIT_USER_AGENT'))
 
-reddit = praw.Reddit(client_id=os.environ.get('REDDIT_CLIENT_ID'),
-                     client_secret=os.environ.get('REDDIT_CLIENT_SECRET'),
-                     user_agent=os.environ.get('REDDIT_USER_AGENT'))
+    headlines = set()
+    for submission in reddit.subreddit('politics').new(limit=None):
+        headlines.add(submission.title)
 
-headlines = set()
-for submission in reddit.subreddit('politics').new(limit=None):
-    headlines.add(submission.title)
+    sia = SIA()
+    results = []
 
-sia = SIA()
-results = []
+    for line in headlines:
+        pol_score = sia.polarity_scores(line)
+        pol_score['headline'] = line
+        results.append(pol_score)
 
-for line in headlines:
-    pol_score = sia.polarity_scores(line)
-    pol_score['headline'] = line
-    results.append(pol_score)
-
-df = pd.DataFrame.from_records(results)
-print(df.head(10))
-df.to_parquet('headlines.parquet')
+    df = pd.DataFrame.from_records(results)
+    df.to_parquet(f'/tmp/headlines-{datetime.now().strftime("%Y-%m-%d_%H%M%S")}.parquet')
+    sleep(1800)
+    sys.exit(0)
