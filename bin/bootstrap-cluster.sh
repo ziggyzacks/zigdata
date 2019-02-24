27e13c6115b9358db9e2b2f714de51ef626324c3
ziggyzacks <zigzacks@gmail.com>
2019-02-24 16:34:22 -0500

    go back to old way

---
+++ bin/bootstrap-cluster.sh
@@ -0,0 +1,8 @@
+# helm setup
+kubectl create serviceaccount --namespace kube-system tiller                                                                                           1 ⏎  +21048 1:44 ❰─┘
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+helm init --service-account tiller
+# make a development namespace
+kubectl create -f https://k8s.io/examples/admin/namespace-dev.json
+# dashboard
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml
