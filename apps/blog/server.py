70d4bb76742075ca61c46cf658ae36f7c18cf04f
ziggyzacks <zigzacks@gmail.com>
2019-02-28 22:03:13 -0500

    try and run sanic

--- apps/blog/server.py
+++ apps/blog/server.py
@@ -1,11 +1,18 @@
 from sanic import Sanic
-from sanic.response import json
+from sanic.response import json, html
 
 app = Sanic()
 
-@app.route('/')
-async def test(request):
+
+@app.route('/json')
+async def api_route(request):
     return json({'hello': 'world'})
+
+
+@app.route('/')
+def frontend_route(request):
+    return html('<p>Hello world!</p>')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888)
