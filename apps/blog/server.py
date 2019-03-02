cf4852178502d7d78d2a81ecedd69d6f9fc9e4ce
ziggyzacks <zigzacks@gmail.com>
2019-03-02 18:08:17 -0500

    try to have sanic serve static files

--- apps/blog/server.py
+++ apps/blog/server.py
@@ -3,16 +3,7 @@ from sanic.response import json, html
 
 app = Sanic()
 
-
-@app.route('/json')
-async def api_route(request):
-    return json({'hello': 'world'})
-
-
-@app.route('/')
-def frontend_route(request):
-    return html('<p>Hello world!</p>')
-
+app.static('/', '/var/www/zigdata')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888)
