c2d9b43a951642d1f0fefc3f03da2204d3f0586b
ziggyzacks <zigzacks@gmail.com>
2019-03-02 18:29:04 -0500

    try and use simple server

--- apps/blog/server.py
+++ apps/blog/server.py
@@ -3,7 +3,16 @@ from sanic.response import json, html
 
 app = Sanic()
 
-app.static('/', '/var/www/zigdata')
+
+@app.route('/json')
+async def api_route(request):
+    return json({'hello': 'world'})
+
+
+@app.route('/')
+def frontend_route(request):
+    return html('<p>Hello world!</p>')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888)
