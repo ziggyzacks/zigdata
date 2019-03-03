1b2dd13a75349350ed014a00ec7b87f140ad7799
ziggyzacks <zigzacks@gmail.com>
2019-03-03 15:42:07 -0500

    clean up set up

--- bin/bootstrap-cluster.sh
+++ bin/bootstrap-cluster.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${KEY_FILE} -out ${CERT_FILE} -subj "/CN=${HOST}/O=${HOST}"
-kubectl create secret tls ${CERT_NAME} --key ${KEY_FILE} --cert ${CERT_FILE}
-
 # helm setup
 kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 helm init --service-account tiller
-# dashboard
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml
-# for debugging networking issues
-kubectl create -f https://k8s.io/examples/admin/dns/busybox.yaml
