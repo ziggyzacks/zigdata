d2753262a13a79393a8bc8d11604a78127345b5d
ziggyzacks <zigzacks@gmail.com>
2019-03-03 01:30:01 -0500

    some cleanup and minimizing cost

--- bin/cluster-up.sh
+++ bin/cluster-up.sh
@@ -2,8 +2,8 @@
 kops create cluster \
         --zones us-east-1a \
         --node-count 1 \
-        --node-size t3.medium \
-        --master-size c5.large \
+        --node-size t3.micro \
+        --master-size t3.micro \
         --ssh-public-key ssh/zigdata_rsa.pub \
         --name $NAME
 
