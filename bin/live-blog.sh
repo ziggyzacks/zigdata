56f69a7f0746507380832bce034807f5308e45e8
ziggyzacks <zigzacks@gmail.com>
2019-03-03 00:54:46 -0500

    using a template + some minor improvements

--- bin/live-blog.sh
+++ bin/live-blog.sh
@@ -3,7 +3,6 @@ cd apps/blog/zigdata
 # constantly rebuild local
 lektor build -O build --watch &
 PID=$!
-echo $PID
 trap "kill -9 $PID" SIGINT SIGTERM
 # start file watcher to hot reload static pages
 python watcher.py
