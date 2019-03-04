4ecc12020157682d795e8faa76d732d84992ce6e
ziggyzacks <zigzacks@gmail.com>
2019-03-04 17:47:59 -0500

    wip

--- apps/jupyter/jupyter_notebook_config.py
+++ apps/jupyter/jupyter_notebook_config.py
@@ -7,6 +7,7 @@ import stat
 c = get_config()
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8989
+c.NotebookApp.base_url = '/notebook'
 c.NotebookApp.allow_root = True
 c.NotebookApp.allow_origin = '*'
 c.NotebookApp.open_browser = False
